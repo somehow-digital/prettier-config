1a77fdd62b47fb81cfc0fcd9ff7b29590db44cfa
Thomas Deinhamer <code@thasmo.dev>
2024-07-24 23:28:17 +0200

    Install german dictionary for cspell

--- cspell.config.js
+++ cspell.config.js
@@ -1,6 +1,7 @@
 export default {
 	dictionaries: [
 		'en_US',
+		'de-de',
 		'softwareTerms',
 		'typescript',
 		'node',
@@ -8,8 +9,30 @@ export default {
 		'css',
 		'bash',
 		'misc',
+		'project',
 	],
-	ignorePaths: ['node_modules', 'package.json', 'pnpm-lock.yaml', '.gitignore'],
+	dictionaryDefinitions: [
+		{
+			addWords: true,
+			name: 'project',
+			path: './dictionary.txt',
+		},
+	],
+	ignorePaths: [
+		'node_modules/',
+		'.git/',
+		'.husky/',
+		'.idea/',
+		'package.json',
+		'pnpm-lock.yaml',
+		'.npmrc',
+		'.nvmrc',
+		'.gitignore',
+		'.gitattributes',
+		'.editorconfig',
+		'.prettierignore',
+	],
+	import: ['@cspell/dict-de-de'],
 	language: 'en',
 	version: '0.2',
 };
